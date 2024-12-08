c38ad8e5f881d2ab04e9b00b92a845b3749cdbe9
Yash Thakur <146983587+yash-thakur-089@users.noreply.github.com>
2024-12-08 22:19:36 +0530

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,7 +5,6 @@ import './Gallery.css';  // Ensure this is correct
 
 const Gallery = () => {
   const images = [
-    "https://drive.google.com/file/d/1cQDpid3EDeKMU4D0sqANp7L-JkMhzU_z/view?usp=sharing",
     "https://www.abhibus.com/blog/wp-content/uploads/2023/04/Kullu-Manali-Trip-in-May-.jpg",
     "https://s7ap1.scene7.com/is/image/incredibleindia/solang-nullah-manali-himachal-pradesh-1-attr-hero?qlt=82&ts=1726730690372",
     "https://www.clubmahindra.com/blog/media/section_images/blog-topic-6530ecb63a76c89.jpg",
