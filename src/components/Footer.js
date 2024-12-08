659f39a082e570ed65e88e98dea7dce367b9144e
Yash Thakur <146983587+yash-thakur-089@users.noreply.github.com>
2024-12-08 22:03:35 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer style={styles.footer}>
       <p style={styles.text}>© 2024 Manali Tour | Developed by Yash 😎</p>
       <div style={styles.socialLinks}>
-        <a href="https://facebook.com" style={styles.link}>Facebook</a>
+        <a href="https://www.facebook.com/YashThakur089/" style={styles.link}>Facebook</a>
         <a href="https://www.instagram.com/your_fella_yash/" style={styles.link}>Instagram</a>
         <a href="https://x.com/Yash_Thakur_089" style={styles.link}>Twitter</a>
       </div>
