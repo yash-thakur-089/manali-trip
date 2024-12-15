e662d51ce88b17651b0d1dd6e604b774cd0762af
Yash Thakur <146983587+yash-thakur-089@users.noreply.github.com>
2024-12-15 11:53:19 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer style={styles.footer}>
-      <p style={styles.text}>© 2024 Manali Tour | Developed by Yash 😎</p>
+      <p style={styles.text}>© 2024 Manali Tour | Developed by Yash</p>
       <div style={styles.socialLinks}>
         <a href="https://www.facebook.com/YashThakur089/" style={styles.link}>Facebook</a>
         <a href="https://www.instagram.com/your_fella_yash/" style={styles.link}>Instagram</a>
